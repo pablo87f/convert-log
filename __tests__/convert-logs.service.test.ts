4d198d61164c93652f7408c8712ce28e4e4bb03e
Pablo Silva <pablo.silva@avenueeco.com>
2022-07-24 17:00:24 -0300

    ✨ convert logs service

---
+++ __tests__/convert-logs.service.test.ts
@@ -0,0 +1,46 @@
+import ConvertLogsService from '../src/services/convert-logs.service.js';
+
+class MockDate extends Date {
+  constructor() {
+    super('2017-12-15T23:01:06.135Z');
+  }
+}
+
+const OriginalDate = Date;
+
+beforeEach(() => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.Date = MockDate;
+});
+afterAll(() => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.Date = OriginalDate;
+});
+
+beforeAll;
+
+describe('Convert Logs', () => {
+  const logLines = [
+    '312|200|HIT|"GET /robots.txt HTTP/1.1"|100.2',
+    '101|200|MISS|"POST /myImages HTTP/1.1"|319.4',
+    '199|404|MISS|"GET /not-found HTTP/1.1"|142.9',
+    '312|200|INVALIDATE|"GET /robots.txt HTTP/1.1"|245.1',
+  ];
+  const expectedConvertedLog = [
+    '#Version: 1.0',
+    `#Date: 15/12/2017 23:01:06`,
+    `#Fields: provider http-method status-code uri-path time-taken response-size cache-status`,
+    `"MINHA CDN" GET 200 /robots.txt 100 312 HIT`,
+    `"MINHA CDN" POST 200 /myImages 319 101 MISS`,
+    `"MINHA CDN" GET 404 /not-found 143 199 MISS`,
+    `"MINHA CDN" GET 200 /robots.txt 245 312 REFRESH_HIT`,
+  ];
+
+  it('should convert log lines successfully', async () => {
+    const convertLogsService = new ConvertLogsService(logLines);
+    const convertedText = await convertLogsService.run();
+    expect(convertedText).toBe(expectedConvertedLog.join('\n'));
+  });
+});
---
+++ src/services/convert-logs.service.ts
@@ -0,0 +1,57 @@
+import { format } from 'date-fns';
+import AgoraLogLine, { HttpMethod } from '../entities/AgoraLogLine.js';
+
+class ConvertLogsService {
+  constructor(private readonly logLines: string[]) {}
+
+  async run() {
+    if (this.logLines) {
+      const minhaNetLogLines = this.logLines.map((line) => {
+        const [responseSize, statusCode, cacheStatus, endpoint, timeTaken] =
+          line.split('|');
+        const [httpMethod, uriPath] = endpoint.replace(/"/g, '').split(' ');
+
+        return {
+          responseSize: parseInt(responseSize),
+          statusCode: parseInt(statusCode),
+          cacheStatus:
+            cacheStatus === 'INVALIDATE' ? 'REFRESH_HIT' : cacheStatus,
+          httpMethod: httpMethod as HttpMethod,
+          uriPath,
+          timeTaken: Math.round(parseFloat(timeTaken)),
+        };
+      });
+
+      const agoraLogLines = minhaNetLogLines.map(
+        ({
+          cacheStatus,
+          httpMethod,
+          responseSize,
+          statusCode,
+          timeTaken,
+          uriPath,
+        }) => {
+          return new AgoraLogLine(
+            'MINHA CDN',
+            httpMethod,
+            statusCode,
+            uriPath,
+            timeTaken,
+            responseSize,
+            cacheStatus,
+          );
+        },
+      );
+
+      const version = `#Version: 1.0\n`;
+      const date = `#Date: ${format(new Date(), 'dd/MM/yyyy HH:mm:ss')}\n`;
+      const fields = `#Fields: provider http-method status-code uri-path time-taken response-size cache-status\n`;
+      const header = `${version}${date}${fields}`;
+
+      return `${header}${agoraLogLines.join('\n')}`;
+    }
+    return undefined;
+  }
+}
+
+export default ConvertLogsService;
